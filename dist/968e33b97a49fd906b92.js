491a7baa8d3f33326c4004d372bde1776e354baa
Andres Morales <arturgeek@gmail.com>
2022-10-24 14:39:39 -0500

    First Load, ready to start

---
+++ dist/968e33b97a49fd906b92.js
@@ -0,0 +1,1 @@
+import"./index.css";import App from"./App";var root=ReactDOM.createRoot(document.getElementById("root"));root.render(React.createElement(React.StrictMode,null,React.createElement(App,null)));
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    resolve: {
+        extensions: ["*",".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                    },
+                ],
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    "css-loader",
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public", "index.html"),
+        }),
+        new MiniCssExtractPlugin({
+            filename: "assets/[name].css",
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "build"),
+        },
+        port: 3000,
+    }
+}
