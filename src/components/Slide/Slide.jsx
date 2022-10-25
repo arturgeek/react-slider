1cc4c04715f8e934fe77e8de3fc4d0522a2c197a
Andres Morales <arturgeek@gmail.com>
2022-10-25 14:48:22 -0500

    Style and handle hover on Slide Component

--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,16 +1,36 @@
 import "./Slide.css";
+import { useState } from "react";
 
 const Slide = ( slideData ) => {
 
-    return <div className="slide" key={slideData.imageUrl}>
-        imageUrl: {slideData.imageUrl}
-        <br/>
-        hoverImageUrl: {slideData.hoverImageUrl}
+    const [hover, setHover] = useState(false);
+    const [background, setBackground] = useState(slideData.imageUrl);
+
+    const handleOnMouseOver = () => {
+        setHover(true);
+        const hoverImageUrl = slideData.hoverImageUrl ?? slideData.imageUrl;
+        setBackground(hoverImageUrl);
+    }
+
+    const handleOnMOuseLeave = () => {
+        setHover(false);
+        setBackground(slideData.imageUrl);
+    }
+
+    return slideData.imageUrl ? <div 
+            className={`slide ${hover ? "hover" : ""}`}
+            key={slideData.imageUrl}
+            style={{
+                backgroundImage: `url("${background}")`
+            }}
+            onMouseOver={handleOnMouseOver}
+            onMouseLeave={handleOnMOuseLeave}
+        >
         <br/>
         linkSpan: {slideData.linkSpan}
         <br/>
         linkUrl: {slideData.linkUrl}
-    </div>;
+    </div> : null;
 };
 
 export default Slide;
