f18a4090155d68c1e63772fd4c9bbbac80e8ad87
Andres Morales <arturgeek@gmail.com>
2022-10-25 15:02:09 -0500

    Add context to change the Slides

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createContext } from 'react';
 import { getSliders } from './API/SlidersData';
 import SliderContainer from './components/SliderContainer/SliderContainer';
+import { ActiveSlideContext } from './Context/SliderContexts';
 
 function App() { 
 
   const [slides, setSlides] = useState([]);
+  const [acitveSlide, setActiveSlide] = useState(0);
   
   useEffect( () => {
     const slides = getSliders();
@@ -13,9 +15,11 @@ function App() {
   }, [] );
 
   return (
-    <div className="App">
-      <SliderContainer slides={slides} />
-    </div>
+    <ActiveSlideContext.Provider value={acitveSlide}>
+      <div className="App">
+        <SliderContainer slides={slides} />
+      </div>
+    </ActiveSlideContext.Provider>
     );
   }
   
--- src/components/Slide/Slide.jsx
+++ src/components/Slide/Slide.jsx
@@ -1,10 +1,12 @@
 import "./Slide.css";
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { ActiveSlideContext } from "../../Context/SliderContexts";
 
 const Slide = ( slideData ) => {
 
     const [hover, setHover] = useState(false);
     const [background, setBackground] = useState(slideData.imageUrl);
+    const acitveSlide = useContext(ActiveSlideContext);
 
     const handleOnMouseOver = () => {
         setHover(true);
@@ -26,10 +28,13 @@ const Slide = ( slideData ) => {
             onMouseOver={handleOnMouseOver}
             onMouseLeave={handleOnMOuseLeave}
         >
-        <br/>
-        linkSpan: {slideData.linkSpan}
-        <br/>
-        linkUrl: {slideData.linkUrl}
+            <div>
+            linkSpan: {slideData.linkSpan}
+            <br/>
+            linkUrl: {slideData.linkUrl}
+            <br/>
+            acitveSlide: {acitveSlide}
+            </div>        
     </div> : null;
 };
 
