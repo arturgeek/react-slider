61651101bde6ab5ad5030ca78cc8dd7c999f6ab2
Andres Morales <arturgeek@gmail.com>
2022-10-25 16:21:28 -0500

    Add the interval funcitonality, required using reducers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import './App.css';
-import { useEffect, useState, createContext } from 'react';
-import { getSliders } from './API/SlidersData';
+import { useEffect, useState, useReducer } from 'react';
+import { getSlides } from './API/SlidersData';
 import SliderContainer from './components/SliderContainer/SliderContainer';
 import { ActiveSlideContext } from './Context/SliderContexts';
 
 function App() { 
-
+  
   const [slides, setSlides] = useState([]);
-  const [acitveSlide, setActiveSlide] = useState(0);
+  const [acitveSlide, setActiveSlide] = useState("");
+  let sliderInterval = null;
+
+  const sliderActionsReducer = (currentSlideIndex, action) => {
+    switch(action.type) {
+      case "NextSlide":
+
+        console.log(currentSlideIndex, slides.length);
+        if( currentSlideIndex >= slides.length ){
+          currentSlideIndex = 0;
+        }
+        console.log(currentSlideIndex);
+        setActiveSlide(slides[ currentSlideIndex ].imageUrl)
+        
+        return currentSlideIndex + 1;
+      default:
+        return currentSlideIndex;
+    }
+  }
+
+  const [currentSlideIndex, dispatch] = useReducer(sliderActionsReducer, 0);
   
   useEffect( () => {
-    const slides = getSliders();
+    const slides = getSlides();
     setSlides(slides);
+    setActiveSlide(slides[ currentSlideIndex ].imageUrl)
+    
+    /*
+    Everytime the component re renders it requires clear the 
+    interval to avoid running more than once
+    */
+    clearInterval(sliderInterval);
+    sliderInterval = setInterval( () => {
+      dispatch({type: "NextSlide"})
+    }, 2000);
   }, [] );
 
+  
   return (
     <ActiveSlideContext.Provider value={acitveSlide}>
-      <div className="App">
-        <SliderContainer slides={slides} />
-      </div>
+    <div className="App">
+    <SliderContainer slides={slides} />
+    </div>
     </ActiveSlideContext.Provider>
     );
   }
